0b4f15876a097debec6fd255679efca0975801e4
Andy Qu <89557223+AndyQu2000@users.noreply.github.com>
2022-09-07 21:09:41 -0400

    Add files via upload

---
+++ 316-Principle of programming language/TINYJ/316java/12.sol
@@ -0,0 +1,19 @@
+<program>
+ <importStmt>
+  Reserved Word: import
+  Reserved Word: java
+  .
+  Reserved Word: util
+  .
+  Reserved Word: Scanner
+  ;
+  ... node has no more children
+ Reserved Word: class
+ IDENTIFIER: CS316ex12
+ {
+ <dataFieldDecl>
+  Reserved Word: static
+  <varDecl>
+   Reserved Word: int
+   <singleVarDecl>
+    ... node has no more children
